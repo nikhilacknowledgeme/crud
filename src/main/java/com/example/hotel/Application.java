d3715cf7a4c0680e1d50e29112a046d1df15b593
nikhil <nikhil.s.paraskar@gmail.com>
2026-03-22 19:13:07 +0530

    added comment

--- src/main/java/com/example/hotel/Application.java
+++ src/main/java/com/example/hotel/Application.java
@@ -14,6 +14,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+//I have the comment
 @SpringBootApplication
 @EnableSwagger2
 public class Application implements WebMvcConfigurer {
